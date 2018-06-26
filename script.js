67798e92816714c01942f25da603ec0f974862e2
alondravelar2 <35636517+alondravelar2@users.noreply.github.com>
2018-06-25 17:34:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -110,8 +110,28 @@ function buildLevel() {
   // create platforms, monsters, and any other game objects
   // best method is to draw sprites from left to right on the screen
   createPlatform(50, 690, 5);
+  createPlatform(850, 645, 3);
+  createPlatform(1450, 595, 4);
+  createPlatform(2050, 480, 2.5);
+  createPlatform(2500, 350, 2);
+  createPlatform(2500, 670, 3);
+  createPlatform(3000, 525, 2);
   createCollectable(300, 340);
+  createCollectable(745, 430);
+  createCollectable(1085, 320);
+  createCollectable(1232, 430);
+  createCollectable(1600, 320);
+  createCollectable(1756, 389);
   createMonster(500, 600, 0);
+  createMonster(1085, 530, 0);
+  createMonster(1730, 470, 0);
+  createMonster(1860, 470, 0);
+  createMonster(500, 600, -1);
+  createMonster(2290, 290, 0);
+  createMonster(2800, 300, 0);
+  goal = createSprite(3100, 415);
+  goal.addImage(goalImage);
+
 }
 
 // Creates a player sprite and adds animations and a collider to it
@@ -142,6 +162,7 @@ function createPlatform(x, y, len) {
       var middle = createSprite(x + (128 * i), y, 0, 0);
       middle.addToGroup(platforms);
       middle.addImage(platformImageMiddle);
+
       //middle.debug = true;
     }
   }
@@ -180,50 +201,101 @@ function createCollectable(x, y) {
 // function calls executeLoss(). If a monster falls off the screen, it is
 // removed from the game.
 function applyGravity() {
-
+    player.velocity.y += GRAVITY;
+    if(player.previousPosition.y !== player.position.y) {
+      playerGrounded = false;
+    }
+    if(player.position.y >= height) {
+    }
+    for(var i = 0; i < monsters.length; i++) {
+      monsters[i].velocity.y += GRAVITY;
+      if(monsters[i].position.y >= height) {
+        monsters[i].remove();
+      }
+    }
 }
 
 // Called in the draw() function. Continuously checks for collisions and overlaps
 // between all relevant game objects. Depending on the collision or overlap that
 // occurs, a specific callback function is run.
 function checkCollisions() {
-
+    player.collide(platforms, platformCollision);
+    player.collide(monsters, playerMonsterCollision);
+    monsters.collide(platforms, platformCollision);
+    player.overlap(collectables, getCollectable);
+    player.overlap(goal, executeWin);
 }
 
 // Callback function that runs when the player or a monster collides with a
 // platform.
 function platformCollision(sprite, platform) {
-
+    if(sprite === player && sprite.touching.bottom) {
+        sprite.velocity.y = 0;
+        playerGrounded = true;
+        currentJumpTime = MAX_JUMP_TIME;
+        currentJumpForce = DEFAULT_JUMP_FORCE;
+    }
+    for(var i = 0; i < monsters.length; i++) {
+      if(sprite === monsters[i] && sprite.touching.bottom) {
+        sprite.velocity.y = 0;
+      }
+    }
 }
 
 // Callback function that runs when the player collides with a monster.
 function playerMonsterCollision(player, monster) {
+  if(player.touching.bottom) {
+ monster.remove();
 
+var defeatedMonster = createSprite(monster.position.x, monster.position.y, 0, 0);
+defeatedMonster.addImage(monsterDefeatImage);
+defeatedMonster.mirrorX(monster.mirrorX());
+defeatedMonster.scale = 0.25;
+defeatedMonster.life = 40;
+currentJumpTime = MAX_JUMP_TIME;
+currentJumpForce = DEFAULT_JUMP_FORCE;
+player.velocity.y = currentJumpForce;
+millis = new Date();
+score++;
+
+   }
+   else {
+     executeLoss();
+   }
 }
 
 // Callback function that runs when the player overlaps with a collectable.
 function getCollectable(player, collectable) {
-
-}
+ collectable.remove();
+ player.score++;
+  }
 
 // Updates the player's position and current animation by calling
 // all of the relevant "check" functions below.
 function updatePlayer() {
-  //console.log("Player x: " + player.position.x + " Player y: " + player.position.y);
-
+  console.log("Player x: " + player.position.x + " Player y: " + player.position.y);
+  checkIdle();
+  checkFalling();
+  checkJumping();
+  checkMovingLeftRight();
 }
 
 // Check if the player is idle. If neither left nor right are being pressed and the
 // player is grounded, set player's animation to "idle", and change her
 // x velocity to 0.
 function checkIdle() {
-
+  if(!keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW) && playerGrounded) {
+    player.changeAnimation("idle");
+    player.velocity.x = 0;
+  }
 }
 
 // Check if the player is falling. If she is not grounded and her y velocity is
 // greater than 0, then set her animation to "fall".
 function checkFalling() {
-
+  if(!playerGrounded && player.velocity.y > 0) {
+    player.changeAnimation("fall");
+  }
 }
 
 // Check if the player is jumping. First, if her y velocity is less than 0, set
@@ -231,28 +303,54 @@ function checkFalling() {
 // key, which should allow her to jump higher so long as currentJumpTime is greater
 // than 0.
 function checkJumping() {
-
+  if(player.velocity.y < 0) {
+    player.changeAnimation("jump");
+    if(keyIsDown(UP_ARROW) && currentJumpTime > 0) {
+      player.velocity.y = currentJumpForce;
+      deltaMillis = new Date();
+      currentJumpTime -= deltaMillis - millis;
+    }
+  }
 }
 
 // Check if the player is moving left or right. If so, move the player character
 // left or right according to DEFAULT_VELOCITY. Also be sure to mirror the
 // player's sprite left or right to avoid "moonwalking".
 function checkMovingLeftRight() {
-
+    if(keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW)) {
+        player.mirrorX(-1);
+        if(playerGrounded) {
+            player.changeAnimation("run");
+        }
+        player.velocity.x = -DEFAULT_VELOCITY;
+    }
+    else if(keyIsDown(RIGHT_ARROW) && !keyIsDown(LEFT_ARROW)) {
+        player.mirrorX(1);
+        if(playerGrounded) {
+            player.changeAnimation("run");
+        }
+        player.velocity.x = DEFAULT_VELOCITY;
+    }
 }
 
 // Check if the player has pressed the up arrow key. If the player is grounded
 // this should initiate the jump sequence, which can be extended by holding down
 // the up arrow key (see checkJumping() above).
 function keyPressed() {
-
+  if(keyCode === UP_ARROW && playerGrounded) {
+    playerGrounded = false;
+    player.velocity.y = currentJumpForce;
+    millis = new Date();
+  }
 }
 
 // Check if the player has released the up arrow key. If the player's y velocity
 // is < 0 (that is, she is currently moving "up" on the canvas), then this will
 // immediately set currentJumpTime to 0, causing her to begin falling.
 function keyReleased() {
-
+  if(keyCode === UP_ARROW && player.velocity.y < 0) {
+    currentJumpTime = 0;
+  }
 }
 
 // Check if the player has typed the "p" key, which pauses the game. We use
@@ -285,13 +383,19 @@ function updateDisplay() {
 
   // turn camera back on
   camera.on();
+  camera.position.x = player.position.x;
 
+  for(var i = 0; i < collectables.length; i++) {
+    collectables[i].rotation += 5;
+  }
 }
 
 // Called when the player has won the game (e.g., reached the goal at the end).
 // Anything can happen here, but the most important thing is that we call resetGame()
 // after a short delay.
 function executeWin() {
+  noLoop();
+  setTimeout(resetGame, 1000);
 
 }
 
@@ -299,5 +403,6 @@ function executeWin() {
 // a monster). Anything can happen here, but the most important thing is that we
 // call resetGame() after a short delay.
 function executeLoss() {
-
+noLoop();
+setTimeout(resetGame, 1000);
 }
